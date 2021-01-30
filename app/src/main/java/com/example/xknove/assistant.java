322c79e789aab7f91e6fc98c801409aa8de50cad
mullagalievdeniz <mullagalievdeniz@yandex.ru>
2021-01-30 21:21:50 +0300

    XKNOVE

---
+++ app/src/main/java/com/example/xknove/assistant.java
@@ -0,0 +1,14 @@
+package com.example.xknove;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class assistant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assistant);
+    }
+}
