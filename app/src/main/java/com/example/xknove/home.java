08e8b3b44028bc9269a1ff88501c94b4cee60e1b
mullagalievdeniz <mullagalievdeniz@yandex.ru>
2021-02-01 21:27:10 +0300

    XKNOVE

--- app/src/main/java/com/example/xknove/home.java
+++ app/src/main/java/com/example/xknove/home.java
@@ -15,7 +15,10 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btn = (Button) findViewById(R.id.button);
     }
+
+
 
     public void onClick(View view) {
         Intent intent = new Intent(home.this, time.class);
